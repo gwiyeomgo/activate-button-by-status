629be26545bb729153185c40c9fe979f7e36e370
gwiyeomgo <77624394+gwiyeomgo@users.noreply.github.com>
2022-11-26 04:48:07 +0900

    #4 add TypeScript to ActivateByStatusReact component (#5)
    #4 #5

--- src/index.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import ActivateButtonByStatus from "./lib/ActivateButtonByStatus";
-
-ReactDOM.render(<ActivateButtonByStatus />, document.getElementById("root"));
---
+++ src/index.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import ActivateButton from "./lib/ActivateButton";
+
+ReactDOM.render(<ActivateButton  type={"status"} title={"activeButton"} currentStatus={"Register"}/>, document.getElementById("root"));
