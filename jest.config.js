43efece40280156a7a2ef7b9b433d8737178037e
gwiyeomgo <gwiyeomgo@gmail.com>
2022-12-21 20:57:18 +0900

    #15 jest setup and modify test case

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+//jest --init  :Generate a basic configuration file
+//https://jestjs.io/docs/getting-started
+
+export default {
+  setupFilesAfterEnv: ['<rootDir>/jest-setup.js'],
+};
--- src/lib/activateButton.spec.tsx
+++ src/lib/activateButton.spec.tsx
@@ -2,12 +2,26 @@ import React from "react";
 import {fireEvent,screen, render} from '@testing-library/react';
 import ActivateButton from "./activateButton";
 import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom/extend-expect';
 
+// describe : test 그룹화
 describe('button click test',  () => {
     //it :  should do this thing
     //test:  if it does this thing
-    test('if render button', () => {
+    it('if render button', () => {
          render(<ActivateButton
+            title={'Click'}
+            type={"status"}
+            onClick={() => {}}
+            currentStatus={"Registered"}
+            activeStatus={["Registered"]}
+        />);
+
+        const button = screen.getByRole('button', {name: 'Click'});
+        expect(button).toBeInTheDocument();
+    });
+    test('button click', () => {
+        render(<ActivateButton
             title={'Click'}
             type={"status"}
             onClick={() => {}}
@@ -18,6 +32,23 @@ describe('button click test',  () => {
         const button = screen.getByRole('button', {name: 'Click'});
         fireEvent.click(button)
         userEvent.click(button)
+
+        expect(button).toHaveProperty('disabled',false)
+    });
+
+    test('button disabled', () => {
+        render(<ActivateButton
+            title={'Click'}
+            type={"status"}
+            onClick={() => {}}
+            disabled={true}
+            currentStatus={"Registered"}
+            activeStatus={["Registered"]}
+        />);
+
+        const button = screen.getByRole('button', {name: 'Click'});
+
+        expect(button).toHaveProperty('disabled',false)
     });
 
 });
